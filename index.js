20040f77a05a1c4801cdd89875d6a94c098ade3c
Whaeedil <baoppyhossen1234@gmail.com>
2024-11-11 21:56:09 +0600

    resolve

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from your frontend URL
+    origin: "https://weedingpro.netlify.app", // Allow requests from your frontend URL
   })
 );
 
